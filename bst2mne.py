3c5a04ce1f812a7a04781278e537b450840e2d78
Dmitriy Altukhov <dm.altukhov@ya.ru>
2016-08-10 10:23:44 -0400

    Finished bst2mne

--- bst2mne.py
+++ bst2mne.py
@@ -3,7 +3,10 @@ from mne.preprocessing import read_ica
 import contextlib
 import sys
 import cStringIO
-
+import scipy.io as sio
+import numpy as np
+from mne import create_info, EpochsArray
+from mne.io.pick import channel_type
 @contextlib.contextmanager
 def nostdout():
     save_stdout = sys.stdout
@@ -13,16 +16,61 @@ def nostdout():
 
 
 @click.command()
-@click.argument('subjdirs', nargs=-1)
-def cli(subjdirs):
+@click.argument('matfiles', nargs=-1)
+@click.option('--savename', '-s', default='foo-epo.fif')
+@click.option('--rec-type', type=click.Choice(['ds', 'fif']), default='ds')
+@click.option('--infosrc', '-i', help='file to take info from')
+def cli(matfiles, savename, rec_type, infosrc):
     """
     Convert brainstorm epochs to mne.Epochs object
     """
-    for fname in subjdirs:
+    if infosrc:
+        if rec_type is 'ds':
+            from mne.io import read_raw_ctf as read_raw
+        elif rec_type is 'fif':
+            from mne.io import Raw as read_raw
         with nostdout():
-            ica = read_ica(fname)
-        n_comp = ica.n_components_
-        tot_var = ica.pca_explained_variance_.sum()
-        n_comp_var = ica.pca_explained_variance_[:n_comp].sum()
-        PVE = n_comp_var / tot_var
-        click.echo(PVE)
+            raw_with_info = read_raw(infosrc)
+
+    isFirst = True
+    for fname in matfiles:
+        with nostdout():
+            mat_epoch = sio.loadmat(fname)
+            # click.echo(mat_epoch)
+        if isFirst:
+            data = mat_epoch['F']
+            times = mat_epoch['Time']
+            # print times[0,-1]
+            isFirst = False
+        else:
+            data = np.dstack((data, mat_epoch['F']))
+        # click.echo(data.shape)
+    data = data.transpose((2,0,1))
+
+
+    n_channels = data.shape[1]
+    sfreq = times.shape[1] / (times[0,-1] + times[0,1])
+    
+    
+    if infosrc:
+        if rec_type is 'ds':
+            from mne.io import read_raw_ctf as read_raw
+        elif rec_type is 'fif':
+            from mne.io import Raw as read_raw
+
+        with nostdout():
+            raw_with_info = read_raw(infosrc)
+        good_info = raw_with_info.info
+        # click.echo(len(good_info['ch_names']))
+
+        ch_types = [channel_type(good_info, idx) for idx in range(n_channels)]
+
+        # click.echo(len(ch_types))
+
+        info = create_info(ch_names=good_info['ch_names'], sfreq=sfreq, ch_types=ch_types)
+    else:
+        info = create_info(n_channels, sfreq)
+
+    with nostdout():
+        epochs = EpochsArray(data, info)
+    epochs.save(savename)
