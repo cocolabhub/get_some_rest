26a6ad88b734678e4a0607ef43d648a6ccb52974
Dmitriy Altukhov <dm.altukhov@ya.ru>
2016-08-18 00:27:46 -0400

    Added examples to psd2bandpower.py

--- psd2bandpower.py
+++ psd2bandpower.py
@@ -75,6 +75,25 @@ def cli(pwr_dirs, band, bandname, destdir):
     Find [basename]-psds.npy and [basename]-freqs.npy in [PWR_DIRS], load them to compute power in a BAND and save the result to [basename][-bandname][_band]-pwr.npy
 
     Each dir must contain only one instance of [basename]-psds.npy and [basename]-freqs.npy 
+
+    EXAMPLES:
+
+        Say, we have a pipeline folder with psds computed. Each subfolder has psds and freqs files inside. The most basic usage would be
+
+        $ psd2bandpwr ~/pipeline/*subj_id* 8 12 
+        
+
+        This will compute power in alpha band for each subject in ~/pipeline
+
+
+
+        Now assume we have two groups: controls and patients and we want to save results in two separate folders. It can be done like this:
+
+        $ psd2bandpwr ~/pipeline/*subj_id_Control* 8 12 -d ~/Controls_alpha
+        
+
+        $ psd2bandpwr ~/pipeline/*subj_id_Patient* 8 12 -d ~/Patients_alpha
+
     """
     # print band
     for pwr_dir in pwr_dirs:
@@ -87,6 +106,8 @@ def cli(pwr_dirs, band, bandname, destdir):
         data = get_band_power(psds, freqs, band)
         if destdir:
             savedir = destdir
+            if not os.path.isdir(destdir):
+                os.mkdir(destdir)
         else:
             savedir = pwr_dir
 
