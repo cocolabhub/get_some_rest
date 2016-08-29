97b48e3d195430a1bef9f9e309968a9eaf4e5a5b
Dmitriy Altukhov <dm.altukhov@ya.ru>
2016-08-29 05:15:14 -0400

    Typo

--- psd2bandpower.py
+++ psd2bandpower.py
@@ -108,7 +108,7 @@ def cli(pwr_dirs, band, bandname, destdir):
         if destdir:
             savedir = destdir
             if not os.path.isdir(destdir):
-                os.makdirs(destdir)
+                os.makedirs(destdir)
         else:
             savedir = pwr_dir
 
